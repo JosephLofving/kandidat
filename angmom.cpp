4fb108a879b6176c39ec0b2b4a56c8f4d04ca34f
Joself <joseph.lofving@gmail.com>
2021-02-19 18:11:55 +0100

    Skrev angmom.triag. Skrev den något mer koncist än i Pythonversionen, men jag har verifierat att de ger samma output

--- angmom.cpp
+++ angmom.cpp
@@ -1,3 +1,12 @@
+#include <cstdlib> // Behövs för abs
+
+bool triag(int a, int b, int ab){ // Kontrollerar |a-b| <= ab <= a+b
+	if ( abs(a - b) <= ab && ab <= a + b ) {
+		return true;
+	}
+	return false;
+}
+
 bool kDelta(QuantumState bra, QuantumState ket, std::vector <std::string> qN){
 	for(std::string s: qN){
 		if (bra.state[s] != ket.state[s]){
