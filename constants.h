9eb4bc04a17bc72297e6f077151d31b73f1736e5
Hanna Olvhammar <hanolv@student.chalmers.se>
2021-02-20 16:17:34 +0100

    Sketch of setup_G0_vector

--- constants.h
+++ constants.h
@@ -3,7 +3,8 @@
 
 // when using constants in another file, use e.g. const::rad2deg
 namespace const {
-	inline constexpr double rad2deg{ 180 / 3.1415926535 }; // C++ verkar inte ha pi smidigt definierat (include <cmath>?)
+	inline constexpr double pi{ 3.1415926535 }; // C++ verkar inte ha pi smidigt definierat (include <cmath>?)
+	inline constexpr double rad2deg{ 180 / pi }; // Don't know if this works, needed pi elsewhere
 }
 
 #endif
--- scattering.cpp
+++ scattering.cpp
@@ -1,6 +1,7 @@
 #include "constants.h" // namespace const
 
-#include <complex> //for complex numbers with std::complex<double>
+#include <complex> // for complex numbers with std::complex<double>, imaginary unit with 
+#include <numeric> // for std::accumulate
 #include <iterator> // for std:size
 #include <tuple>
 #include <cmath>
@@ -12,24 +13,34 @@
 //-------------------------------------- HANNA'S ------------------------------------------------
 
 
-// What should this return? Return void in meantime
-// k (array) is quadrature points (was "p" in python), w (array) is weights, k0 (double?) is on-shell-point
-void setup_G0_vector(double k[], double w[], double k0[]) // OBS: Passing arrays changes the array even outside function (something with pointers...)
+/* k (array) is quadrature points (was "p" in python), w (array) is weights, k0 (double?) is on-shell-point
+ * OBS: Passing arrays changes the array even outside function (something with pointers...)
+ * This function needs to return a pointer... unsure at the moment
+ */
+double* setup_G0_vector(double k[], double k0[], double w[]) 
 { 
-	int N{ std::size(k) }; // since k has N elements k_j for j=1,...,N
-	std::complex<double> D[2*(N+1)]{ }; // standard to let all elements be zero if not specified
-										// WHY 2*(N+1) rather than N+1?
-
-	//Equation (2.22)
-	//For the first N values let D have elements...?
-	D[N]=
+	int N{ std::size(k) };					// since k has N elements k_j for j=1,...,N
+	std::complex<double> D[2 * (N + 1)]{ }; // standard to let all elements be zero if not specified
+										    // WHY 2*(N+1) rather than N+1?
 
 
+    /* Now: Equation (2.22)
+	 * OBS: Chose own theory before python script here, so it differs.
+	 * Difference: In python first part of (2.22) is D[0,N] and second part of (2.22) is D[0],
+	 * and we use 2*2*mu/pi as constant while python script does not
+	 */
 
+	double mu{ 1 }; // reduced mass, from constants, set to 1 since it does not exist yet
+	double pre_factor{ (2 / const::pi) * 2 * mu };
 
+	D[1, N] = - pre_factor * k * k * w / (k0 * k0 - k * k);					// first part of (2.22)
+	double d[N]{ w / (k0 * k0 - k * k };									// makes next line shorter
+	D[0] = pre_factor * k0 * k0 * std::accumulate(d.begin(), d.end(), 0);	// second part of  (2.22)
+	D[0] += 2 * mu * 1i * k0;
+	D[N + 1, 2 * (N + 1)] = D[0, N + 1];									// make a copy of theoretical D and 
+																			// store in second part of array D
 
-
-
+	return D;
 }
 
 // setup_VG_kernel
@@ -39,7 +50,9 @@ void setup_G0_vector(double k[], double w[], double k0[]) // OBS: Passing arrays
 
 
 
+
 //----------------------------------------JOSEPH'S------------------------------------------------------------------
+
 std::tuple<double, double, double> blattToStapp(double deltaMinusBB, double deltaPlusBB, double twoEpsilonJBB) {
 	double twoEpsilonJ = asin(sin(twoEpsilonJBB) * sin(deltaMinusBB - deltaPlusBB));
 	double deltaMinus = 0.5 * (deltaPlusBB + deltaMinusBB + asin(tan(twoEpsilonJ) / tan(twoEpsilonJBB)))* const::rad2deg;
