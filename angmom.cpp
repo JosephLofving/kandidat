038bf55fbc40b75d5837400ec668aacf00e09349
Hanna Olvhammar <hanolv@student.chalmers.se>
2021-02-20 10:45:12 +0100

    Ännu mer koncis angmom.triag

--- angmom.cpp
+++ angmom.cpp
@@ -1,10 +1,7 @@
 #include <cstdlib> // Behövs för abs
 
 bool triag(int a, int b, int ab){ // Kontrollerar |a-b| <= ab <= a+b
-	if ( abs(a - b) <= ab && ab <= a + b ) {
-		return true;
-	}
-	return false;
+	return (abs(a - b) <= ab && ab <= a + b);
 }
 
 bool kDelta(QuantumState bra, QuantumState ket, std::vector <std::string> qN){
