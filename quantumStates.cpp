79b12e9bf79b87eef65ff9e696f9063cfb0bcf4d
Alexander Korner <r.edna@live.se>
2021-02-22 10:04:36 +0100

    Commit to avoid conflict

--- quantumStates.cpp
+++ quantumStates.cpp
@@ -75,14 +75,7 @@ class Basis {
 
 };
 
-bool kDelta(QuantumState bra, QuantumState ket, std::vector <std::string> qN){
-	for(std::string s: qN){
-		if (bra.state[s] != ket.state[s]){
-			return false;
-		}
-	}
-	return true;
-}
+
 
 int main()
 {
