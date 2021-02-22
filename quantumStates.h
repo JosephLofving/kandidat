9095a70e67f004a1d3ec29f5e447b16ac49e43bb
skrompf <erikbz@outlook.com>
2021-02-22 09:03:43 +0100

    la till denna

---
+++ quantumStates.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector> 
+#include <iterator>
+#include <math.h> 
+#include <list>
+
+class QuantumState {
+	void addQuantumNumber(string key, int value)
+	void printState()
+};
+class Basis {
+	Basis(int j2min, int j2max, int tzmin, int tzmax)
+	void printBasis()
+};
+
+bool kDelta(QuantumState bra, QuantumState ket, std::vector <std::string> qN)
+int setup_NN_channels()
