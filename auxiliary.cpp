0f11bcbb533dfb24597a7b91b96b5aaa1ec40e40
skrompf <erikbz@outlook.com>
2021-02-23 11:28:52 +0100

    tog bort auxiliary, la till i lapackapi

--- auxiliary.cpp
+++
@@ -1,9 +0,0 @@
-//denna funktion behövs inte...
-void matprint(int mat[][]) {
-	col_maxes = 
-		for (col:mat) {
-			for (x : col) {
-				int col_maxes = 
-			}
-	}
-}
---
+++ lapackAPI.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <stdlib.h>
+
+//denna funktion behövs inte...
+void matprint(int mat[][]) {
+	col_maxes =
+		for (col : mat) {
+			for (x : col) {
+				int col_maxes =
+			}
+		}
+}
