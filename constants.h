b8d5f251c54ac612fc2365a7254992c7e0bc6a1c
skrompf <erikbz@outlook.com>
2021-02-23 08:56:33 +0100

    test

--- constants.h
+++ constants.h
@@ -1,8 +1,6 @@
 #ifndef CONSTANTS_H //när man importerar denna så kollar den så att man inte redan har importerat den. Så kompilatorn inte laddar den flera gånger.
 #define CONSTANTS_H //tror denna namnger kodblocket
 
-// when using constants in another file, use e.g. const::rad2deg
-
 namespace constants {
 	inline constexpr double pi{ 3.1415926535 };			 // C++ verkar inte ha pi smidigt definierat (include <cmath>?)
 	inline constexpr double rad2deg{ 180 / pi };		 // Don't know if this works, needed pi elsewhere
