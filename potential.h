8d4b47b916e89b07e1ad8554f3174a119f21cfa4
skrompf <erikbz@outlook.com>
2021-02-23 16:54:03 +0100

    test

--- potential.h
+++ potential.h
@@ -1,1 +1,5 @@
-#pragma once
+#pragma once //denna gör tydligen så koden bara inkluderas en gång. Vet ej hur nödvändig den är, men bra att veta att kommandot finns typ.
+#include "chiral_LO.h"
+
+std::vector<double>;
+std::vector<double> potential(int argc, char* argv[]);
