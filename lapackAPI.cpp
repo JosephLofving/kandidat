26f33be062e70c3fc983806d3ca0362eee3906c0
skrompf <erikbz@outlook.com>
2021-02-23 11:38:07 +0100

    lapackapi

--- lapackAPI.cpp
+++ lapackAPI.cpp
@@ -5,10 +5,14 @@
 
 //denna funktion behövs inte...
 void matprint(int mat[][]) {
-	col_maxes =
-		for (col : mat) {
-			for (x : col) {
-				int col_maxes =
-			}
-		}
+	//col_maxes =
+		//for (col : mat) {
+			//for (x : col) {
+				//int col_maxes =
+			//}
+		//}
+};
+
+int[][] lapackmat_to_mat(std::vector(int), int i, int j) {
+
 }
