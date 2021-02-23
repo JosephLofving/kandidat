415a095016f41688d9051d4c81223d5b13fb375d
Hanna Olvhammar <hanolv@student.chalmers.se>
2021-02-23 16:51:51 +0100

    Changed types and some equations

--- mesh.cpp
+++ mesh.cpp
@@ -1,17 +1,16 @@
 //#include <boost/math/quadrature/gauss.hpp> //vi skriver den själva istälet
 #include "constants.h"
+#include "lapackAPI.h"
+
 #include <iostream>
 #include <cmath>
-#include <lapackAPI.h>
-
-class arr {
-
-};
+#include <vector>
 
 
-struct tuple {
-	arr t;
-	arr u;
+
+struct Two_vectors {
+	std::vector<double> v1;
+	std::vector<double> v2;
 };
 
 tuple leggauss(N) {
@@ -25,24 +24,33 @@ tuple leggauss(N) {
 
 }
 
-tuple gauss_legendre_line_mesh(int N, int a, int b) {
-	tuple X = leggauss(N);
-	arr x = X[0];
-	arr w = X[1];
-	arr t = 0.5 * (x + 1) * (b - a) + a;
-	arr u = w * 0.5 * (b - a);
-	tuple T = new tuple(t, u);
-	return T;
+
+/* This follows equation (2.18) and (2.19) with the same notation */
+Two_vectors gauss_legendre_line_mesh(int N, int a, int b) { // Change to struct?
+	Two_vectors X = leggauss(N);
+	std::vector<double> p = X[0];
+	std::vector<double> w_prime = X[1];
+
+	/* Translate p and w_prime values for [-1,1] to a general 
+	 * interval [a,b]  for quadrature points k and weights w */  
+
+	double C{ 1 }; // what is the value of this parameter? Choose later?
+	// for loops for these operations?
+	std::vector<double> k = (1 + p) / (1 - p); // 0.5 * (p + 1) * (b - a) + a;
+	std::vector<double> w = 2 * C * w_prime / pow(1 - k, 2);//  w_prime * 0.5 * (b - a);
+	Two_vectors k_and_w{ k, w };
+
+	return k_and_w;
 }
 
-tuple gauss_legendre_inf_mesh(int N, double scale = 100.0) {
-	tuple X = leggauss(N);
-	arr x = X[0];
-	arr w = X[1];
+Two_vectors gauss_legendre_inf_mesh(int N, double scale = 100.0) {
+	Two_vectors X = leggauss(N);
+	std::vector<double> x = X[0];
+	std::vector<double> w = X[1];
 	double pi_over_four = constants::pi;
-	arr t = scale * tan(pi_over_four) * (x + 1.0);
-	arr u = scale * pi_over_four / cos(pi_over_four) * pow(x * 1.0, 2 * w);
-	tuple T = new tuple(t, u);
+	std::vector<double> t = scale * tan(pi_over_four) * (x + 1.0);
+	std::vector<double> u = scale * pi_over_four / cos(pi_over_four) * pow(x * 1.0, 2 * w);
+	Two_vectors T = new Two_vectors(t, u);
 	return T;
 
 }
