883c31cf990c8310acbbac91dc6a05294b251645
Hanna Olvhammar <hanolv@student.chalmers.se>
2021-02-26 09:36:17 +0100

    Changed mesh.h

--- mesh.h
+++ mesh.h
@@ -1,7 +1,19 @@
+#include "constants.h"
+#include "lapackAPI.h"
 #include <iostream>
-//#include <boost/math/quadrature/gauss.hpp>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <numeric> //for std::iota
+#include <random>
 
-struct two_vectors
-two_vectors leggauss(N);
-two_vectors gauss_legendre_line_mesh(int N, int a, int b);
-two_vectors gauss_legendre_inf_mesh(int N, double scale = 100.0);
+
+std::vector<double> elementwise_mult(std::vector<double> v1, std::vector<double> v2);
+double absmax(std::vector<double> vec);
+Two_vectors leggauss(int N);
+std::vector<double> legval(std::vector<double> x, std::vector<double> c);
+std::vector<double> legder(std::vector<double> c);
+LapackMat* legcompanion(std::vector<double> c);
+Two_vectors gauss_legendre_line_mesh(int N, int a, int b);
+Two_vectors gauss_legendre_inf_mesh(int N, double scale = 100.0);
