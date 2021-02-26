4f462936b854232511d2a6c584d1bd54aa0e44ac
Hanna Olvhammar <hanolv@student.chalmers.se>
2021-02-26 11:33:23 +0100

    test

--- test.cpp
+++ test.cpp
@@ -24,7 +24,7 @@ int main() {
 	for (int i; i < N; i++)
 	{
 		double function{}; // function of k[i]
-		sum += k[i] * w[i];
+		sum += function * w[i];
 	}
 	std::cout << "The integral evaluates to approximately " << sum;
 
