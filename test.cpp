aa1c14187b666b225a125b84c8f22171a6f6a600
skrompf <erikbz@outlook.com>
2021-02-26 11:24:33 +0100

    void main

---
+++ test.cpp
@@ -0,0 +1,15 @@
+﻿#include "constants.h"
+#include "lapackAPI.h"
+#include "mesh.h"
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <numeric> //beh�vs f�r std::iota
+#include <random>
+
+void main(int N) {
+	std::cout << "hej";
+}
