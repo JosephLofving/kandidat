4f020c88a601b1d44b71a3acce782ed10649168c
skrompf <erikbz@outlook.com>
2021-02-26 11:38:01 +0100

    test

--- test.cpp
+++ test.cpp
@@ -21,7 +21,7 @@ int main() {
 	std::vector<double> w{ k_and_w.v2 };
 
 	double sum{};
-	for (int i; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
 		double function{sin(k[i])/k[i]}; // function of k[i]
 		sum += function * w[i];
