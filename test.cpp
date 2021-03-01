d195ea54f990de3aeaa34dfaf1d24a5127890d98
skrompf <erikbz@outlook.com>
2021-03-01 10:01:34 +0100

    ändrade till gaussintegralen

--- test.cpp
+++ test.cpp
@@ -24,7 +24,7 @@ int main() {
 	for (int i = 0; i < N; i++)
 	{
 		double function{exp(-k[i]*k[i])}; // function of k[i]
-		sum += function * w[i];
+		sum += function * w[i]; //Integralen = sqrt(pi)/2 = 0.886227
 	}
 	std::cout << "The integral evaluates to approximately " << sum << std::endl;
 
