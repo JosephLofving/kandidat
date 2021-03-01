4a9ecef4a3b543184cd6fe0fe88daa5479d00584
Hanna Olvhammar <hanolv@student.chalmers.se>
2021-03-01 11:20:05 +0100

    Clearer code, comments and variable names

--- constants.h
+++ constants.h
@@ -2,12 +2,12 @@
 #define CONSTANTS_H //tror denna namnger kodblocket
 
 namespace constants {
-	inline constexpr double pi{ 3.1415926535 };			 // C++ verkar inte ha pi smidigt definierat (include <cmath>?)
-	inline constexpr double rad2deg{ 180 / pi };		 // Don't know if this works, needed pi elsewhere
-	inline constexpr double mp{ 938.27208816 };			 // protonmassa (MeV/c^2)
-	inline constexpr double mn{ 939.56542052 };			 // neutronmassa (MeV/c^2)
-	inline constexpr double mN{ (mp + mn) / 2 };		 // nucleon average mass
-	inline constexpr double uN{ (mp * mn) / (mp + mn) }; // nucleon reduced mass
+	inline constexpr double pi{ 3.1415926535 }; // C++ verkar inte ha pi smidigt definierat (include <cmath>?)
+	inline constexpr double rad2deg{ 180 / pi };		 
+	inline constexpr double proton_mass{ 938.27208816 };														  // protonmassa (MeV/c^2)
+	inline constexpr double neutron_mass{ 939.56542052 };													   	  // neutronmassa (MeV/c^2)
+	inline constexpr double nucleon_average_mass { (proton_mass + neutron_mass) / 2 };							  // nucleon average mass
+	inline constexpr double nucleon_reduced_mass { (proton_mass * neutron_mass) / (proton_mass + neutron_mass) }; // nucleon reduced mass
 }
 // when using constants in another file, use e.g. constants::rad2deg
 
--- scattering.cpp
+++ scattering.cpp
@@ -2,85 +2,95 @@
 #include "quantumStates.h"
 #include "lapackAPI.h"
 
-#include <complex>		// for complex numbers with std::complex<double>,
+#include <complex>		
 #include <tuple>
 #include <cmath>
 #include <vector>
 #include <iostream>
 #include <tuple>
 
+
+/* About this file:
+ * 1. Here k is used for quadrature points, whereas the Python code uses p.
+ * 2. Here the G0 vector has size N + 1, whereas the Python code has a G0 vector
+ *    of size 2 * (N + 1). In Python, the last N + 1 elements are copied from 
+ *    the first N + 1. This was used to facilitate use for coupled states, but 
+ *    in this project coupling should be taken into consideration when needed instead. */
+
+
+
+
 inline constexpr std::complex<double> I{ (0.0,1.0) }; // ugly way to get imaginary unit, does not work in all code
 
 
+/* To get the reduced mass, check the isospin channel to determine the type of scattering */
+double get_reduced_mass(std::vector<QuantumState> channel)
+{
+	int tz_channel{ channel[0].state["tz"] };
+	if (tz_channel == -1)	  // Proton-proton scattering
+		return constants::proton_mass / 2;
+	else if (tz_channel == 0) // Proton-neutron scattering
+		return constants::nucleon_reduced_mass;
+	else if (tz_channel == 1) // Peutron-neutron scattering
+		return constants::neutron_mass / 2;
+}
 
-//__________HANNA'S________________
-/* k (vector) is quadrature points (was "p" in python), w (vector) is weights, k0 (double) is on-shell-point.
- * Passes vector of type complex double. */
+
+/* k is quadrature points (was "p" in python), w is weights, k0 is on-shell-point. */
 std::vector<std::complex<double>> setup_G0_vector(std::vector<double> k, std::vector<double> w, double k0)
 { 
-	int N = k.size();								  // since k has N elements k_j for j=1,...,N
-	std::vector<std::complex<double>> D(2 * (N + 1)); // standard to let all elements be zero if not specified
-												      // WHY 2*(N+1) rather than N+1?
+	int N = k.size();							  // k has N elements k_j for j = 1,...,N
+	std::vector<std::complex<double>> D( N + 1 ); // Setup D vector, size N + 1
 
 
     /* Now: Equation (2.22)
-	 * OBS: Chose own theory before python script here, so it differs.
+	 * OBS: Chose own theory before python script here
 	 * Difference: In python first part of (2.22) is D[0,N] and second part of (2.22) is D[0],
 	 * and we use 2*2*mu/pi as constant while python script does not 
-	 * (probably because mu is not a constant? Defined in next function) */
+	 * (probably because mu is not a constant? Defined in next function) 
+	 * ---
+	 * CONCLUSION: Beware that on shell element is in index 0, NOT N+1 as in Python script */
 
-	double mu{ 1 }; // reduced mass, from constants, set to 1 since it does not exist yet
-	double pre_factor{ (2 / constants::pi) * 2 * mu };
+
+	double pre_factor{ (2.0 / constants::pi) }; // Will multiply by 2.0 * mu in setup_VG_kernel
 
 	double sum{}; // for D[0]
 	for (int ind{ 0 }; ind <= N; ind++)
 	{
-		D[ind+1]= -pre_factor * pow(k[ind],2) * w[ind] / (pow(k0,2) - pow(k[ind],2));   // define D[1,N] with k and w vectors
-		D[(N + 1) + ind + 1] = D[ind + 1];												// copy first half of D to second half
-
-		sum += w[ind] / (k0 * k0 - k[ind] * k[ind]);									// to use in D[0]
+		D[ind + 1] = -pre_factor * pow(k[ind], 2) * w[ind] / (pow(k0, 2) - pow(k[ind], 2));   // Define D[1,N] with k and w vectors
+		sum += w[ind] / (k0 * k0 - k[ind] * k[ind]);										  // Use in D[0]
 	}
 	
-	D[0] = pre_factor * k0 * k0 * sum + 2 * mu * I * k0;
+	D[0] = pre_factor * pow(k0, 2) * sum + I * k0;
 	
 	return D;
 }
 
 
 
-//___________HANNA'S______________
-
-//what is "channel"?
 LapackMat setup_VG_kernel(std::vector<QuantumState> channel, std::string key, LapackMat V, std::vector<double> k, std::vector<double> w, double k0)
 {
-	double mu{};
-	int tz_channel{ channel[0].state["tz"] }; // have not changed since QuantumState changed
-	if (tz_channel == -1)
-		mu = constants::mp / 2;
-	else if (tz_channel == 0)
-		mu = constants::uN;
-	else if (tz_channel == 1)
-		mu = constants::mN / 2;
+	double mu{ get_reduced_mass(channel) };
 
-	double Np = k.size();
-	int number_of_blocks = channel.size();
-	std::cout << "Setting up G_0(k0) in channel " << key;
+	std::cout << "Setting up G0(k0) in channel " << key;
 
-	double Np_channel = static_cast<int>(std::sqrt(number_of_blocks) * (Np + 1));
-	std::vector<std::complex<double>> G0{ setup_G0_vector(k, w, k0) };
+	int N = k.size();
+	int number_of_blocks = channel.size();									 // TODO: What does this do?
+	int N_channel = static_cast<int>(std::sqrt(number_of_blocks) * (N + 1)); // TODO: What does this do?
+	
+	std::vector<std::complex<double>> G0{ setup_G0_vector(k, w, k0) };		 // G0 has dimension N+1 here, as opposed to Python code
 
-	// copy G0 up to Np_channel:th (or (Np_channel-1):th?) element
+	/* Copy G0 up to (N_channel-1):th element */
 	std::vector<std::complex<double>> G0_part{};
-	for (int index{ 0 }; index < Np_channel; ) { G0_part[index] = G0[index]; }  // potential off-by-one error here
+	for (int index{ 0 }; index < N_channel; index++) { G0_part[index] = G0[index] * 2.0 * mu; } // May not work since G0_part not fixed size
 
-	// From here, functions from Lapack are needed... Not done yet
+	/* Create VG by initializing empty quadratic matrix and then using VG[i,j] = V[i,j] * G[j] */
 	LapackMat VG = LapackMat(G0_part.size());
-
 	for (int row{ 0 }; row < G0_part.size(); row++)
 	{
 		for (int column{ 0 }; column < G0_part.size(); column++)
 		{
-			VG.setElement(row, column, V.getElement(row, column) * G0_part[column] * 2.0 * mu);
+			VG.setElement(row, column, V.getElement(row, column) * G0_part[column] );
 		}
 	}
 
@@ -88,17 +98,15 @@ LapackMat setup_VG_kernel(std::vector<QuantumState> channel, std::string key, La
 }
 
 
+/* Use equation ??? */
+LapackMat computeTMatrix(std::vector<QuantumState> NN_channel, std::string key, LapackMat V, std::vector<double> k, std::vector<double> w, double k0) 
+{
+	LapackMat VG = setup_VG_kernel(NN_channel, key, V, k, w, k0);
 
+	LapackMat identity = LapackMat(VG.width);
+	LapackMat constants_matrix = (2.0 / constants::pi) * identity;
 
-
-// __________JOSEPH'S____________
-LapackMat computeTMatrix(std::vector<QuantumState> NN_channel, std::string key, LapackMat V, std::vector<double> k, std::vector<double> w, double k0) {
-	LapackMat VG = setup_VG_kernel(NN_channel,key, V, k, w, k0);
-
-	LapackMat VG2 = LapackMat(VG.width);
-	LapackMat VG3 = (2.0/constants::pi) * VG;
-
-	LapackMat IVG = VG2 - VG3;
+	LapackMat IVG = identity - constants_matrix; // WHY? Diagonal elements 1 - 2/pi?
 
 	LapackMat T = solveMatrixEq(IVG, V); // IVG*T = V
 
@@ -107,20 +115,20 @@ LapackMat computeTMatrix(std::vector<QuantumState> NN_channel, std::string key, 
 
 
 
-
-std::vector<std::complex<double>> blattToStapp(std::complex<double> deltaMinusBB, std::complex<double> deltaPlusBB, std::complex<double> twoEpsilonJBB) {
+/* TODO: Explain this theory */
+std::vector<std::complex<double>> blattToStapp(std::complex<double> deltaMinusBB, std::complex<double> deltaPlusBB, std::complex<double> twoEpsilonJBB) 
+{
 	std::complex<double> twoEpsilonJ = asin(sin(twoEpsilonJBB) * sin(deltaMinusBB - deltaPlusBB));
 
-	std::complex<double> deltaMinus = 0.5 * (deltaPlusBB + deltaMinusBB + asin(tan(twoEpsilonJ) / tan(twoEpsilonJBB)))*constants::rad2deg;
-	std::complex<double> deltaPlus = 0.5 * (deltaPlusBB + deltaMinusBB - asin(tan(twoEpsilonJ) / tan(twoEpsilonJBB)))*constants::rad2deg;
-	std::complex<double> epsilon = 0.5 * twoEpsilonJ*constants::rad2deg;
+	std::complex<double> deltaMinus = 0.5 * (deltaPlusBB + deltaMinusBB + asin(tan(twoEpsilonJ) / tan(twoEpsilonJBB))) * constants::rad2deg;
+	std::complex<double> deltaPlus = 0.5 * (deltaPlusBB + deltaMinusBB - asin(tan(twoEpsilonJ) / tan(twoEpsilonJBB))) * constants::rad2deg;
+	std::complex<double> epsilon = 0.5 * twoEpsilonJ * constants::rad2deg;
 
 	return { deltaMinus, deltaPlus, epsilon };
 }
 
 
 
-//__________HANNA'S________________
 // T should be matrix, maybe multidimensional vector? Let NN_channel be MD vector
 std::vector<std::complex<double>> compute_phase_shifts(std::vector<QuantumState> NN_channel,std::string key, double k0, LapackMat T)
 {
