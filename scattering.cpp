06a89cbdaa662b391bf513aef5924a66230af4d2
Hanna Olvhammar <hanolv@student.chalmers.se>
2021-03-04 09:46:50 +0100

    Tried python code in setup_G0_vector

--- scattering.cpp
+++ scattering.cpp
@@ -45,18 +45,21 @@ double get_reduced_mass(std::vector<QuantumState> channel)
 std::vector<std::complex<double>> setup_G0_vector(std::vector<double> k, std::vector<double> w, double k0)
 { 
 	int N = k.size();							// k has N elements k_j for j = 1,...,N
-	std::vector<std::complex<double>> D(N + 1); // Setup D vector, size N + 1
+	std::vector<std::complex<double>> D(2* (N + 1)); // Setup D vector, size N + 1
 
     /* Equation (2.22) is used to set elements in D. */
-	double pre_factor{ 1.0 };//(2.0 / constants::pi) }; // Later we will also multiply by 2.0 * mu (in setup_VG_kernel)
+	//double pre_factor{ 1.0 };//(2.0 / constants::pi) }; // Later we will also multiply by 2.0 * mu (in setup_VG_kernel)
 	double sum{}; // for D[0]
 	for (int ind{ 0 }; ind < N; ind++)
 	{
-		D[ind] = -pre_factor * pow(k[ind], 2) * w[ind] / (pow(k0, 2) - pow(k[ind], 2));   // Define D[1,N] with k and w vectors
+		D[ind] = pow(k[ind], 2) * w[ind] / (pow(k0, 2) - pow(k[ind], 2));   // Define D[1,N] with k and w vectors
+		D[ind + (N+1)] = D[ind];
+		
 		sum += w[ind] / (k0 * k0 - k[ind] * k[ind]);										  // Use in D[0]
 	}
 	
-	D[N] = -pre_factor * pow(k0, 2) * sum - constants::pi/2 * I * k0;
+	D[N] = -pow(k0, 2) * sum - (constants::pi/2) * I * k0;
+	D[2 *( N + 1) - 1] = D[N];
 	
 	return D;
 }
