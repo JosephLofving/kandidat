f72eb57987396a55c34e732bc26a969c53d0987b
Hanna Olvhammar <hanolv@student.chalmers.se>
2021-03-06 16:48:30 +0100

    First attempt at using own theory in this file, does not give correct answer yet

--- scattering.cpp
+++ scattering.cpp
@@ -22,13 +22,20 @@
 
 
 
-inline constexpr std::complex<double> I(0.0,1.0); // Temporary way to get imaginary unit in this file
+// TODO:
+// Coupling
+// Constants pi/2
+// constants mu
+// return to own theory
 
-//std::complex<double> I(0,1);
+// Plan for coupling: isCoupled function and then a local copy when needed
+
+
+inline constexpr std::complex<double> I(0.0,1.0); 
+
 
 /* To get the reduced mass, check the isospin channel to determine the type of scattering */
-double get_reduced_mass(std::vector<QuantumState> channel)
-{
+double get_reduced_mass(std::vector<QuantumState> channel) {
 	int tz_channel{ channel[0].state["tz"] };
 	if (tz_channel == -1)	  // Proton-proton scattering
 		return constants::proton_mass / 2;
@@ -36,79 +43,55 @@ double get_reduced_mass(std::vector<QuantumState> channel)
 		return constants::nucleon_reduced_mass;
 	else if (tz_channel == 1) // Neutron-neutron scattering
 		return constants::neutron_mass / 2;
-	std::cout << "Incorrect tz_channel";
+
+	std::cout << "Incorrect tz_channel"; // TODO: This error handling should maybe be done sooner?
 	abort();
 }
 
 
+/* If there is only one channel the state is uncoupled, otherwise there are four channels and the state is coupled. */
+bool isCoupled(std::vector<QuantumState> NN_channel) {
+	return !(NN_channel.size() == 1);
+}
 
+
+// OBS!!! SETUP_G0 HAS A NEW INPUT: NN_CHANNEL
 /* k is quadrature points (was "p" in python), w is weights, k0 is on-shell-point. */
-std::vector<std::complex<double>> setup_G0_vector(std::vector<double> k, std::vector<double> w, double k0)
-{ 
-	int N = k.size();							// k has N elements k_j for j = 1,...,N
-	std::vector<std::complex<double>> D(2* (N + 1)); // Setup D vector, size N + 1
+std::vector<std::complex<double>> setup_G0_vector(std::vector<QuantumState> NN_channel, std::vector<double> k, std::vector<double> w, double k0) { 
+	int N = k.size();  // k has N elements k_j for j = 1,...,N
+	std::vector<std::complex<double>> D(N + 1);
 
     /* Equation (2.22) is used to set elements in D. */
-	double pi_over_two{ (constants::pi / 2.0) }; // Later we will also multiply by 2.0 * mu (in setup_VG_kernel)
+	double mu = get_reduced_mass(NN_channel);
+	double two_over_pi = (2.0 / constants::pi);
 	double sum{}; // for D[0]
-	for (int ind{ 0 }; ind < N; ind++)
-	{
-		D[ind] = w[ind] * pow(k[ind], 2) / (pow(k0, 2) - pow(k[ind], 2));   // Define D[1,N] with k and w vectors
-		D[ind + (N+1)] = D[ind];
-		
-		sum += w[ind] * pow(k0, 2) / (pow(k0, 2) - pow(k[ind], 2));          // Use in D[0]
+	for (int i{ 0 }; i < N; i++) {
+		D[i] = - two_over_pi * 2.0 * mu * pow(k[i], 2) * w[i] / (pow(k0, 2) - pow(k[i], 2)); // Define D[1,N] with k and w vectors
+		sum += w[i] / (pow(k0, 2) - pow(k[i], 2));										   // Used in D[0]
 	}
 	
-	D[N] = - sum - pi_over_two * I * k0;
-	D[2 *( N + 1) - 1] = D[N];
+	D[N] = two_over_pi * 2.0 * mu * pow(k0, 2) * sum + 2.0 * mu * I * k0; // In the theory, this element is placed at index 0
 
 	return D;
 }
 
-///* k is quadrature points (was "p" in python), w is weights, k0 is on-shell-point. */
-//std::vector<std::complex<double>> setup_G0_vector(std::vector<double> k, std::vector<double> w, double k0)
-//{
-//	int N = k.size();							// k has N elements k_j for j = 1,...,N
-//	std::vector<std::complex<double>> D(N + 1); // Setup D vector, size N + 1
-//
-//	/* Equation (2.22) is used to set elements in D. */
-//	double pre_factor{ (2.0 / constants::pi) }; // Later we will also multiply by 2.0 * mu (in setup_VG_kernel)
-//	double sum{}; // for D[0]
-//	for (int ind{ 0 }; ind < N; ind++)
-//	{
-//		D[ind + 1] = -pre_factor * pow(k[ind], 2) * w[ind] / (pow(k0, 2) - pow(k[ind], 2));   // Define D[1,N] with k and w vectors
-//		sum += w[ind] / (k0 * k0 - k[ind] * k[ind]);										  // Use in D[0]
-//	}
-//
-//	D[0] = pre_factor * pow(k0, 2) * sum + I * k0;
-//
-//	return D;
-//}
 
 
-LapackMat setup_VG_kernel(std::vector<QuantumState> NN_channel, std::string key, LapackMat V, std::vector<double> k, std::vector<double> w, double k0)
-{
+LapackMat setup_VG_kernel(std::vector<QuantumState> NN_channel, std::string key, LapackMat V, std::vector<double> k, std::vector<double> w, double k0) {
 	std::cout << "Setting up G0(k0) in channel " << key << std::endl;
+	std::vector<std::complex<double>> G0{ setup_G0_vector(NN_channel, k, w, k0) };
 
-	int N = k.size();
-	int number_of_blocks = NN_channel.size();								 // Is either 1 (uncoupled) or 4 (coupled)
-	int N_channel = static_cast<int>(std::sqrt(number_of_blocks) * (N + 1)); // If coupled, use second half of G0
-	std::vector<std::complex<double>> G0{ setup_G0_vector(k, w, k0) };		 // G0 has dimension N+1 here, as opposed to Python code
-
-	/* Copy G0 up to (N_channel-1):th element */
-	double mu{ get_reduced_mass(NN_channel) };
-	std::vector<std::complex<double>> G0_part(N_channel);
-	for (int index{ 0 }; index < N_channel; index++) { G0_part[index] = G0[index] * 2.0 * mu; } // * 2.0 * mu 
-
+	/* If coupled, append G0 to itself to facilitate calculations. This means the second half of G0 is a copy of the first. */
+	if (isCoupled(NN_channel)) {
+		G0.insert(std::end(G0), std::begin(G0), std::end(G0)); // TODO: Risk that this does not work properly, might want to test
+	}
 
 	/* Create VG by initializing identity matrix and then using VG[i,j] = V[i,j] * G[j] */
-	LapackMat VG = LapackMat(G0_part.size());
+	LapackMat VG = LapackMat(G0.size());
 
-	for (int row{ 0 }; row < G0_part.size(); row++)
-	{
-		for (int column{ 0 }; column < G0_part.size(); column++)
-		{
-			VG.setElement(row, column, V.getElement(row, column) * G0_part[column]);
+	for (int row{ 0 }; row < G0.size(); row++) {
+		for (int column{ 0 }; column < G0.size(); column++) {
+			VG.setElement(row, column, V.getElement(row, column) * G0[column]);
 		}
 	}
 
@@ -117,18 +100,15 @@ LapackMat setup_VG_kernel(std::vector<QuantumState> NN_channel, std::string key,
 
 
 
-/* Use equation ??? */
-LapackMat computeTMatrix(std::vector<QuantumState> NN_channel, std::string key, LapackMat V, std::vector<double> k, std::vector<double> w, double k0) 
-{
-	std::cout << "Solving for the complex T-matrix in channel " << key << ":";
+/* Equation (2.24): [F][T] = [V]. */
+LapackMat computeTMatrix(std::vector<QuantumState> NN_channel, std::string key, LapackMat V, std::vector<double> k, std::vector<double> w, double k0)  {
+	std::cout << "Solving for the complex T-matrix in channel " << key << std::endl;
 
 	LapackMat VG = setup_VG_kernel(NN_channel, key, V, k, w, k0);
-
 	LapackMat identity = LapackMat(VG.width);
-	LapackMat two_over_pi_VG = (2.0 / constants::pi) * VG;
-	LapackMat IVG = identity - two_over_pi_VG;
+	LapackMat F = identity + VG; // Definition in equation (2.25)
 
-	LapackMat T = solveMatrixEq(IVG, V); // IVG*T = V
+	LapackMat T = solveMatrixEq(F, V);
 
 	return T;
 }
@@ -149,18 +129,15 @@ std::vector<std::complex<double>> blattToStapp(std::complex<double> deltaMinusBB
 
 
 /* TODO: Explain theory for this. */
-std::vector<std::complex<double>> compute_phase_shifts(std::vector<QuantumState> NN_channel,std::string key, double k0, LapackMat T)
-{
+std::vector<std::complex<double>> compute_phase_shifts(std::vector<QuantumState> NN_channel,std::string key, double k0, LapackMat T) {
 	std::cout << "Computing phase shifts in channel " << key << std::endl;
 
 	std::vector<std::complex<double>> phases{};
-	int number_of_blocks = NN_channel.size(); // 1 if uncoupled, 4 if coupled
 	double mu{ get_reduced_mass(NN_channel) };
-	double factor{ 2 * mu * k0 }; 
+	double rho_T =  2 * mu * k0; // Equation (2.27) in the theory
 	int N{ T.width };
 
-	if (number_of_blocks > 1) // if coupled 
-	{
+	if (isCoupled(NN_channel)) {
 		N = static_cast<int>( (N - 2) / 2);				// WHY?
 		std::complex<double> T11 = T.getElement(N,N);
 		std::complex<double> T12 = T.getElement(2 * N + 1, N);
@@ -168,27 +145,23 @@ std::vector<std::complex<double>> compute_phase_shifts(std::vector<QuantumState>
 
 		/* Blatt - Biedenharn(BB) convention */
 		std::complex<double> twoEpsilonJ_BB{ std::atan(2.0 * T12 / (T11 - T22)) };
-		std::complex<double> delta_plus_BB{ -0.5 * I * std::log(1.0 - I * factor * (T11 + T22) + I * factor * (2.0 * T12) / std::sin(twoEpsilonJ_BB)) };
-		std::complex<double> delta_minus_BB{ -0.5 * I * std::log(1.0 - I * factor * (T11 + T22) - I * factor * (2.0 * T12) / std::sin(twoEpsilonJ_BB)) };
+		std::complex<double> delta_plus_BB{ -0.5 * I * std::log(1.0 - I * rho_T * (T11 + T22) + I * rho_T * (2.0 * T12) / std::sin(twoEpsilonJ_BB)) };
+		std::complex<double> delta_minus_BB{ -0.5 * I * std::log(1.0 - I * rho_T * (T11 + T22) - I * rho_T * (2.0 * T12) / std::sin(twoEpsilonJ_BB)) };
 
 		std::vector<std::complex<double>> phases_append{ blattToStapp(delta_minus_BB, delta_plus_BB, twoEpsilonJ_BB) };
 
 		phases.push_back(phases_append[0]);
 		phases.push_back(phases_append[1]);
 		phases.push_back(phases_append[2]);
-
-//std::cout << "\nDELTA_PLUS: " << delta_plus << "\n";
-//std::cout << "\nDELTA_MINUS: " << delta_minus << "\n";
 	}
-	else
-	{
+	/* This completely follows the theory in equation (2.26). */
+	else {
 		N -= 1;
-		std::complex<double> T_element = T.getElement(N, N);
-		std::complex<double> Z = 1.0 - factor * 2 * I * T_element;
-		std::complex<double> delta{ (-0.5 * I) * std::log(Z) * constants::rad2deg };
+		std::complex<double> T0 = T.getElement(N, N);
+		std::complex<double> argument = 1.0 - 2.0 * I * rho_T * T0;
+		std::complex<double> delta = (-0.5 * I) * std::log(argument) * constants::rad2deg;
      
 		phases.push_back(delta);
-//std::cout << "\nDELTA: " << delta << "\n";
 	}
 
 	return phases;
