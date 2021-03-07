21e0b4ea5d3ec90bb074781de27ad8e36569245e
Joself <joseph.lofving@gmail.com>
2021-03-07 14:00:50 +0100

    Kommenterade computeTMatrix

--- scattering.cpp
+++ scattering.cpp
@@ -2,7 +2,7 @@
 #include "quantumStates.h"
 #include "lapackAPI.h"
 
-#include <complex>		
+#include <complex>
 #include <tuple>
 #include <cmath>
 #include <vector>
@@ -13,9 +13,9 @@
 /* About this file:
  * 1. Here k is used for quadrature points, whereas the Python code uses p.
  * 2. Here the G0 vector has size N + 1, whereas the Python code has a G0 vector
- *    of size 2 * (N + 1). In Python, the last N + 1 elements are copied from 
- *    the first N + 1. This was used to facilitate use for coupled states, but 
- *    in this project coupling should be taken into consideration when needed instead. 
+ *    of size 2 * (N + 1). In Python, the last N + 1 elements are copied from
+ *    the first N + 1. This was used to facilitate use for coupled states, but
+ *    in this project coupling should be taken into consideration when needed instead.
  * 3. Here the on shell point is in the zeroth element of G0, whereas it is in
  *    element N + 1 in the Python code. */
 
@@ -44,7 +44,7 @@ double get_reduced_mass(std::vector<QuantumState> channel)
 
 /* k is quadrature points (was "p" in python), w is weights, k0 is on-shell-point. */
 std::vector<std::complex<double>> setup_G0_vector(std::vector<double> k, std::vector<double> w, double k0)
-{ 
+{
 	int N = k.size();							// k has N elements k_j for j = 1,...,N
 	std::vector<std::complex<double>> D(2* (N + 1)); // Setup D vector, size N + 1
 
@@ -55,10 +55,10 @@ std::vector<std::complex<double>> setup_G0_vector(std::vector<double> k, std::ve
 	{
 		D[ind] = w[ind] * pow(k[ind], 2) / (pow(k0, 2) - pow(k[ind], 2));   // Define D[1,N] with k and w vectors
 		D[ind + (N+1)] = D[ind];
-		
+
 		sum += w[ind] * pow(k0, 2) / (pow(k0, 2) - pow(k[ind], 2));          // Use in D[0]
 	}
-	
+
 	D[N] = - sum - pi_over_two * I * k0;
 	D[2 *( N + 1) - 1] = D[N];
 
@@ -98,7 +98,7 @@ LapackMat setup_VG_kernel(std::vector<QuantumState> NN_channel, std::string key,
 	/* Copy G0 up to (N_channel-1):th element */
 	double mu{ get_reduced_mass(NN_channel) };
 	std::vector<std::complex<double>> G0_part(N_channel);
-	for (int index{ 0 }; index < N_channel; index++) { G0_part[index] = G0[index] * 2.0 * mu; } // * 2.0 * mu 
+	for (int index{ 0 }; index < N_channel; index++) { G0_part[index] = G0[index] * 2.0 * mu; } // * 2.0 * mu
 
 
 	/* Create VG by initializing identity matrix and then using VG[i,j] = V[i,j] * G[j] */
@@ -118,7 +118,16 @@ LapackMat setup_VG_kernel(std::vector<QuantumState> NN_channel, std::string key,
 
 
 /* Use equation ??? */
-LapackMat computeTMatrix(std::vector<QuantumState> NN_channel, std::string key, LapackMat V, std::vector<double> k, std::vector<double> w, double k0) 
+/* Computes the T-matrix
+   @param NN_channel The scattering channel
+   @param key The channel key (?)
+   @param V The potential matrix
+   @param k The quadrature points from the Gauss-Legendre mesh
+   @param w The weights of the quadrature points in k
+   @param k0 The scattering momentum
+   @return T-matrix
+*/
+LapackMat computeTMatrix(std::vector<QuantumState> NN_channel, std::string key, LapackMat V, std::vector<double> k, std::vector<double> w, double k0)
 {
 	std::cout << "Solving for the complex T-matrix in channel " << key << ":";
 
@@ -136,7 +145,7 @@ LapackMat computeTMatrix(std::vector<QuantumState> NN_channel, std::string key, 
 
 
 /* TODO: Explain theory for this. */
-std::vector<std::complex<double>> blattToStapp(std::complex<double> deltaMinusBB, std::complex<double> deltaPlusBB, std::complex<double> twoEpsilonJBB) 
+std::vector<std::complex<double>> blattToStapp(std::complex<double> deltaMinusBB, std::complex<double> deltaPlusBB, std::complex<double> twoEpsilonJBB)
 {
 	std::complex<double> twoEpsilonJ = std::asin(std::sin(twoEpsilonJBB) * std::sin(deltaMinusBB - deltaPlusBB));
 
@@ -156,10 +165,10 @@ std::vector<std::complex<double>> compute_phase_shifts(std::vector<QuantumState>
 	std::vector<std::complex<double>> phases{};
 	int number_of_blocks = NN_channel.size(); // 1 if uncoupled, 4 if coupled
 	double mu{ get_reduced_mass(NN_channel) };
-	double factor{ 2 * mu * k0 }; 
+	double factor{ 2 * mu * k0 };
 	int N{ T.width };
 
-	if (number_of_blocks > 1) // if coupled 
+	if (number_of_blocks > 1) // if coupled
 	{
 		N = static_cast<int>( (N - 2) / 2);				// WHY?
 		std::complex<double> T11 = T.getElement(N,N);
@@ -186,7 +195,7 @@ std::vector<std::complex<double>> compute_phase_shifts(std::vector<QuantumState>
 		std::complex<double> T_element = T.getElement(N, N);
 		std::complex<double> Z = 1.0 - factor * 2 * I * T_element;
 		std::complex<double> delta{ (-0.5 * I) * std::log(Z) * constants::rad2deg };
-     
+
 		phases.push_back(delta);
 //std::cout << "\nDELTA: " << delta << "\n";
 	}
