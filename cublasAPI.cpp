f88b436ac5b3457b6dec6ae89fec3228590afda0
Joself <joseph.lofving@gmail.com>
2021-03-31 17:20:11 +0200

    Påbörjade en omskrivning till tensorer för cuBLAS

---
+++ cublasAPI.cpp
@@ -0,0 +1,57 @@
+#include "cublasAPI.h"
+
+void Tensor::init(int x, int y, int z) {
+		width = x;
+		height = y;
+		matrixAmt = z;
+		contents = std::vector<std::complex<double>>(x*y*z, 0.0);
+}
+
+Tensor::Tensor(int x, int y, int z) {
+	Tensor::init(x, y, z);
+}
+
+Tensor::Tensor(int x) {
+	Tensor::init(x, x, 1); // Creates a zero-filled matrix with given size.
+
+	for (int i = 0; i < x; i++) {
+		this->setElement(0, i, i, 1.0); // Sets all diagonal elements to 1.0.
+	}
+}
+
+std::complex<double> Tensor::getElement(int matrixNum, int row, int col) {
+	if (row >= this->height) { // Ensures valid row index. Prints both indices for debugging purposes.
+		std::cout << "Invalid row: " << row << " (Column: " << col << ")" << std::endl;
+		abort();
+	}
+	if (col >= this->width) { // Ensures valid column index. Prints both indices for debugging purposes.
+		std::cout << "Invalid column: " << col << " (Row: " << row << ")" << std::endl;
+		abort();
+	}
+	return contents[matrixNum*this->width*this->height + row + col*height]; // The element at the given indices. Note that the matrix is in column major order.
+}
+
+void Tensor::setElement(int matrixNum, int row, int col, std::complex<double> value) {
+	if (row >= this->height) { // Ensures valid row index. Prints both indices for debugging purposes.
+		std::cout << "Invalid row: " << row << " (Column: " << col << ")" << std::endl;
+		abort();
+	}
+	if (col >= this->width) { // Ensures valid column index. Prints both indices for debugging purposes.
+		std::cout << "Invalid column: " << col << " (Row: " << row << ")" << std::endl;
+		abort();
+	}
+	contents[matrixNum*this->width*this->height + row + col*height] = value; // Note that the matrix is in column major order.
+}
+
+void Tensor::print(int matrixNum) {
+	std::cout.precision(2); // Amount of decimals to be printed.
+	std::cout << std::scientific; // Ensures scientific notation.
+
+	for (int currentRow = 0; currentRow < height; currentRow++) {
+		for (int currentCol = 0; currentCol < width; currentCol++) {
+			std::complex<double> element = getElement(matrixNum, currentRow, currentCol);
+			std::cout << element.real() << " + " << element.imag() << "i \t"; // Prints the real and imaginary part and aligns to terminal tabs
+    	}
+    	std::cout << std::endl; // Line break for new row.
+  	}
+}
---
+++ cublasAPI.h
@@ -0,0 +1,43 @@
+#ifndef CUBLASAPI_H
+#define CUBLASAPI_H
+
+#include <iostream>
+#include <vector>
+#include <string> // Behövs för numberToFixedWidth()
+#include <complex>
+
+// A simple class for LAPACK-matrices
+class Tensor {
+public:
+	int width;  // Amount of columns in each matrix
+	int height; // Amount of rows in each matrix
+	int matrixAmt; // Amount of matrices
+	std::vector<std::complex<double>> contents; // Contents of all matrices in column-major order
+
+	Tensor(int x, int y, int z); // Default constructor. Creates a zero-filled tensor
+	Tensor(int x); // Creates an identity matrix with given dimension
+
+	std::complex<double> getElement(int matrixNum, int row, int col); // Returns the element at given indices
+	void setElement(int matrixNum, int row, int col, std::complex<double> value); // Sets the element at given indices
+
+	void print(int matrixNum); // Prints the matrix with the given number
+
+    // friend Tensor operator+(Tensor A, Tensor B);  // Matrix addition. A+B
+	// friend Tensor operator-(Tensor A, Tensor B);  // Matrix subtraction. A-B
+	// friend Tensor operator*(std::complex<double> scalar, Tensor A); // Scalar multiplication. scalar*A
+	// friend Tensor operator*(Tensor A, std::complex<double> scalar); // Scalar multiplication. A*scalar
+	// friend Tensor operator*(Tensor A, Tensor B);  // Matrix multiplication. A*B
+
+private:
+	void init(int x, int y, int z); // Function called by all constructors. Avoids duplicate code
+};
+
+struct TwoVectors { // Struct for storing two related vectors
+	std::vector<double> v1;
+	std::vector<double> v2;
+};
+
+// Tensor solveMatrixEq(Tensor A, Tensor B); // Matrix solver. Solves AX=B and returns X
+// std::vector<double> eigenValues(Tensor A); // Finds the eigenvalues of a symmetric real matrix
+
+#endif
