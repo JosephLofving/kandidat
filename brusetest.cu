d6bc7fba9947e274072d894b0577f3e2b61a6028
skrompf <erikbz@outlook.com>
2021-04-13 10:18:37 +0200

    testfil

---
+++ brusetest.cu
@@ -0,0 +1,36 @@
+#include "mesh.h"
+#include "scattering.h"
+#include "potential.h"
+#include <fstream>
+#include <iomanip>
+#include <cuComplex.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <curand_kernel.h>
+
+void setupVG(double* k)
+{	
+	int matrixHeight = 100;
+	k[0] *= 100;
+}
+
+
+int main() {
+	int N = 100;
+	TwoVectors k_and_w = gaussLegendreInfMesh(100, 100);
+
+	std::vector<double> kVect = k_and_w.v1;
+	double* k = &kVect[0];
+	double* k_dev;
+	cudaMalloc((void**)&k_dev, N * sizeof(double));
+	cudaMemcpy(&k_dev, &k, N * sizeof(double), cudaMemcpyHostToDevice);
+
+	setupVG << <1, 1 >> > (k_dev);
+
+	cudaDeviceSynchronize();
+
+	//std::cout << cuCreal(&k_dev[0]);
+	cudaFree(k_dev);
+
+	return 0;
+}
