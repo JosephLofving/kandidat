192f10d8c5fd23c3badd34039fd2a458a9ead2ac
Joself <joseph.lofving@gmail.com>
2021-04-13 09:10:23 +0200

    Städade lite

--- main.cu
+++ main.cu
@@ -6,7 +6,6 @@
 
 #include <cuComplex.h>
 
-// Borde vara V^T * G0 iställer
 __global__
 void setupVG(double *k, double *w, cuDoubleComplex *V, double k0, cuDoubleComplex *G0, cuDoubleComplex *VG, int matrixHeight)
 {
@@ -18,16 +17,6 @@ void setupVG(double *k, double *w, cuDoubleComplex *V, double k0, cuDoubleComple
 }
 
 int main() {
-	/*
-	std::ofstream myfile;
-    myfile.open ("data.csv");
-
-	myfile << "Real av fasskift";
-	myfile << ",";
-	myfile << "N";
-	myfile << "\n";
-	*/
-
 	std::vector<QuantumState> base = setupBase(0, 2, 0, 2);
     std::map<std::string, std::vector<QuantumState> > channels = setupNNChannels(base);
 	printChannels(channels);
@@ -50,35 +39,20 @@ int main() {
 //-------------------------- FOR GPU --------------------------------
 //------------------------------------------------------------------
 
-	std::vector<double> k;
-	std::vector<double> w;
-
-
-
 	double k0 = getk0(channel, Tlab);
 
+	TwoVectors k_and_w = gaussLegendreInfMesh(NKvadratur, scale);
 
+	std::vector<double> kVect = k_and_w.v1;
+	double* k = &kVect[0];
+	std::vector<double> wVect = k_and_w.v2;
+	double* w = &wVect[0];
 
-	// Allocate Unified Memory, i.e. let the following objects be accessible
-	// from both GPU and CPU
+std::vector<std::complex<double>> G0_std = setupG0Vector(channel, kVect, wVect, k0);
 
+	int N = kVect.size();
 
-	// Initialize k, w, V_matrix on CPU
-	TwoVectors k_and_w{ gaussLegendreInfMesh(NKvadratur, scale) };
-	k = k_and_w.v1;
-	w = k_and_w.v2;
-
-std::vector<std::complex<double>> G0_std = setupG0Vector(channel, k, w, k0);
-
-	int N = k.size();
-
-	double *k_dev;
-	double *w_dev;
-	cuDoubleComplex *V_dev;
-	cuDoubleComplex *G0_dev;
-	cuDoubleComplex *VG_dev;
-
-	LapackMat V_matrix = potential(channel, k, Tlab);
+	LapackMat V_matrix = potential(channel, kVect, Tlab);
 	cuDoubleComplex *V = new cuDoubleComplex[N*N];
 
 	for (int i = 0; i < V_matrix.height*V_matrix.width; i++) {
@@ -90,6 +64,12 @@ std::vector<std::complex<double>> G0_std = setupG0Vector(channel, k, w, k0);
 	for(int i = 0; i < G0_std.size(); i++){
 		G0[i] = make_cuDoubleComplex(G0_std[i].real(), G0_std[i].imag());
 	}
+
+	double k_dev;// = (double*) malloc(N*sizeof(double));
+	double w_dev;// = (double*) malloc(N*sizeof(double));
+	cuDoubleComplex *V_dev;
+	cuDoubleComplex *G0_dev;
+	cuDoubleComplex *VG_dev;
 
 	cudaMalloc((void**)&k_dev, N*sizeof(double));
 	cudaMalloc((void**)&w_dev, N*sizeof(double));
@@ -104,25 +84,18 @@ std::vector<std::complex<double>> G0_std = setupG0Vector(channel, k, w, k0);
 
 	setupVG<<<1, 1>>>(k_dev, w_dev, V_dev, k0, G0_dev, VG_dev, N);
 
-	cuDoubleComplex VG[N*N];
-
-	cudaMemcpy(&VG, &VG_dev, N*N*sizeof(cuDoubleComplex), cudaMemcpyDeviceToHost);
-
-	// Compute the phase shifts for many different T matrices
-	//std::vector<std::complex<double>> phase = computePhaseShifts<<<1,1>>>(channel, key, k0, T);
-
-	// Wait for GPU to finish before accessing on host
 	cudaDeviceSynchronize();
+
+	cuDoubleComplex VG[N*N];
+	cudaMemcpy(&VG, &VG_dev, N*N*sizeof(cuDoubleComplex), cudaMemcpyDeviceToHost);
 
 	std::cout << cuCreal(VG[0]);
 
-
-	// Free memory from Unified Memory
-	// cudaFree(VG_dev);
-	// cudaFree(V_dev);
-	// cudaFree(G0_dev);
-	// cudaFree(k_dev);
-	// cudaFree(w_dev);
+	cudaFree(VG_dev);
+	cudaFree(V_dev);
+	cudaFree(G0_dev);
+	cudaFree(k_dev);
+	cudaFree(w_dev);
 
 	return 0;
 }
