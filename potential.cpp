7632dd14668949d4bf473fb9b72b8d23c1c42a72
Alexander Korner <r.edna@live.se>
2021-04-19 09:04:29 +0200

    Added coupled channels to potential

--- potential.cpp
+++ potential.cpp
@@ -1,6 +1,39 @@
 
 #include "potential.h"
 
+int getArrayIndex(QuantumState state){
+    /* Array to be filled with potential elements. The ordering of the elements are:
+     * V_array[0]="V_S0": S=0, L=LL=J
+     * V_array[1]="V_S1": S=1, L=LL=J
+     * V_array[2]="V_pp": S=1, L=J+1, LL=J+1
+     * V_array[3]="V_mm": S=1, L=J-1, LL=J-1
+     * V_array[4]="V_pm": S=1, L=J+1, LL=J-1
+     * V_array[5]="V_mp": S=1, L=J-1, LL=J+1
+     * So the 1S0-element will be given by V_array[0] */
+    if(state.state["s"]==0){
+         return 0;
+     }
+    if(state.state["l"]==state.state["ll"]){
+        if(state.state["l"]==state.state["j"]){
+            return 1;
+        }
+        if(state.state["l"]==state.state["j"]+1){
+            return 2;
+        }
+        if(state.state["l"]==state.state["j"]-1){
+            return 3;
+        }
+     }
+
+     if(state.state["l"]>state.state["ll"]){
+         return 4;
+     }
+    else{
+        return 5;
+    }
+}
+
+
 cuDoubleComplex* potential(std::vector<QuantumState> channel, double* k, double Tlab, double* k0, int NKvadratur) {
 //std::vector<double> potential(int argc, char* argv[]){
     
@@ -12,15 +45,30 @@ cuDoubleComplex* potential(std::vector<QuantumState> channel, double* k, double 
     potentialClassPtr = new chiral_LO();
 
     double* VArray = new double [6];
-    bool coupled = false;
+    bool coupled;
+    
 
     double* kNew = new double[NKvadratur + 1];
     for (int i = 0; i < NKvadratur; ++i) {
         kNew[i] = k[i];
     }
     kNew[NKvadratur] = k0;
-    cuDoubleComplex* VMatrix = new cuDoubleComplex[(NKvadratur + 1) * (NKvadratur + 1)];
 
+    if(channel.size()>1){
+        coupled = true;
+        cuDoubleComplex* VMatrix = new cuDoubleComplex[(NKvadratur + 1)*2 * (NKvadratur + 1)*2];
+    }
+
+    else{
+        coupled = false;
+        cuDoubleComplex* VMatrix = new cuDoubleComplex[(NKvadratur + 1) * (NKvadratur + 1)];
+    }
+    
+
+
+    int arrayIndex;
+    int rowIndex=0;
+    int colIndex=0;
     for (QuantumState state : channel){
         int L = state.state["l"];
         int S = state.state["s"];
@@ -28,10 +76,33 @@ cuDoubleComplex* potential(std::vector<QuantumState> channel, double* k, double 
         int T = state.state["t"];
         int Tz = state.state["tz"];
 
+        arrayIndex=getArrayIndex(state);
+
+        if(arrayIndex==2){
+            rowIndex = 1;
+            colIndex = 1;
+        }
+
+        if(arrayIndex==3){
+            rowIndex = 0;
+            colIndex = 0;
+        }
+
+        if(arrayIndex==4){
+            rowIndex = 0;
+            colIndex = 1;
+        }
+
+        if(arrayIndex==5){
+            rowIndex = 1;
+            colIndex = 0;
+        }
+
+
         for (int kIn = 0; kIn < NKvadratur + 1; kIn++) {
             for (int kOut = 0; kOut < NKvadratur + 1; kOut++) {
                 potentialClassPtr->V(kNew[kIn], kNew[kOut], coupled, S, J, T, Tz, VArray);
-                VMatrix[(kIn)+(kOut * (NKvadratur + 1))] = make_cuDoubleComplex(constants::pi / 2.0 * VArray[0], 0);
+                VMatrix[(kIn+rowIndex*(NKvadratur + 1))+(kOut+colIndex*(NKvadratur + 1)) * (NKvadratur + 1)] = make_cuDoubleComplex(constants::pi / 2.0 * VArray[0], 0);
             }
         }
     }
