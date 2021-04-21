024024d83d54d4ff3edc90836fa5da787c1e2c11
Alexander Körner <kornera@vera1.int.private>
2021-04-21 14:58:33 +0200

    Vmatris blir rätt för okopplade fall

--- potential.cu
+++ potential.cu
@@ -1,9 +1,6 @@
 
 #include "potential.h"
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <curand_kernel.h>
-#include <cuComplex.h>
+
 
 int getArrayIndex(QuantumState state){
     /* Array to be filled with potential elements. The ordering of the elements are:
@@ -49,8 +46,6 @@ void potential(cuDoubleComplex* VMatrix, std::vector<QuantumState> channel, doub
     double* VArray = new double [6];
     
     int arrayIndex;
-    int rowIndex=0;
-    int colIndex=0;
     for (QuantumState state : channel){
         int L = state.state["l"];
         int S = state.state["s"];
@@ -60,73 +55,44 @@ void potential(cuDoubleComplex* VMatrix, std::vector<QuantumState> channel, doub
 
         arrayIndex=getArrayIndex(state);
 
-        if(arrayIndex==2){
-            rowIndex = 1;
-            colIndex = 1;
-        }
-
-        if(arrayIndex==3){
-            rowIndex = 0;
-            colIndex = 0;
-        }
-
-        if(arrayIndex==4){
-            rowIndex = 0;
-            colIndex = 1;
-        }
-
-        if(arrayIndex==5){
-            rowIndex = 1;
-            colIndex = 0;
-        }
-
- //       	printf("\n MAIN_2 V_h[0] = %.10e", cuCreal(V_h[0]));
-	//printf("\n MAIN_2 V_h[1] = %.10e", cuCreal(V_h[1]));
-	//printf("\n MAIN_2 V_h[2] = %.10e", cuCreal(V_h[2]));
-	//printf("\n MAIN_2 V_h[3] = %.10e", cuCreal(V_h[3]));
-	//printf("\n MAIN_2 V_h[4] = %.10e", cuCreal(V_h[4]));
-	//printf("\n MAIN_2 V_h[5] = %.10e", cuCreal(V_h[5]));
-
-        /*
-        for (int kIn = 0; kIn < quadratureN + 1; kIn++) {
-            for (int kOut = 0; kOut < quadratureN + 1; kOut++) {
-                potentialClassPtr->V(kNew[kIn], kNew[kOut], coupled, S, J, T, Tz, VArray);
-                VMatrix[(kIn+rowIndex*(quadratureN + 1))+(kOut+colIndex*(quadratureN + 1)) * (quadratureN + 1)] = make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0);
-            }
-        }*/
+        //Creates Vmatrix without on-shell
         cuDoubleComplex* VMat2 = new cuDoubleComplex[matLength * matLength];
-        for (int kIn = 0; kIn < quadratureN; kIn++) {
-            for (int kOut = 0; kOut < quadratureN; kOut++) {
+        for (int kIn = 0; kIn < matLength-1; kIn++) {
+            for (int kOut = 0; kOut < matLength-1; kOut++) {
                 potentialClassPtr->V(k[kIn], k[kOut], coupled, S, J, T, Tz, VArray);
-                VMat2[(kIn + rowIndex * (quadratureN + 1)) + (kOut + colIndex * (quadratureN + 1)) * (quadratureN + 1)] =
-                        make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0);
-            }
-        }
-        for (int i = 0; i < matLength * matLength; ++i) {
-            for (int energyIndex = 0; energyIndex < TLabLength; ++energyIndex) {
-                VMatrix[i + (energyIndex * matLength * matLength)] = VMat2[i];
-                //printf("V2[%i]= %.4e\n",i,VMat2[i]);
+                setElement(VMat2, kIn, kOut, 0, matLength, make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0));
             }
         }
 
+        // Copies Vmatrix for each energy
+        for (int row = 0; row < matLength-1; ++row) {
+            for (int col = 0; col < matLength-1; ++col) {
+                for (int energyIndex = 0; energyIndex < TLabLength; ++energyIndex) {
+                    setElement(VMatrix,row,col,energyIndex,matLength,getElement(VMat2,row,col,0,matLength));
+            }
+        }
+
+        //Sets on-shell points for each energy
         for (int energyIndex=0; energyIndex < TLabLength; ++energyIndex){
-            for (int kOut = 0; kOut < matLength-1; ++kOut) {
-                    potentialClassPtr->V(k0[energyIndex], k[kOut], coupled, S, J, T, Tz, VArray);
-                    VMatrix[((quadratureN + 1) + rowIndex * (quadratureN + 1)) + (kOut * (quadratureN + 1) + colIndex * (quadratureN + 1)) * (quadratureN + 1)+energyIndex*matLength*matLength] =
-                        make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0);
 
+            //Sets on-shell points for last row
+            for (int col = 0; col < matLength-1; ++col) {
+                potentialClassPtr->V(k0[energyIndex], k[col], coupled, S, J, T, Tz, VArray);
+                setElement(VMatrix,matLength-1,col,energyIndex,matLength,make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0));
             }
-            for (int kIn = 0; kIn < matLength-1; ++kIn) {
-                potentialClassPtr->V(k[kIn], k0[energyIndex], coupled, S, J, T, Tz, VArray);
-                VMatrix[(kIn+ rowIndex * (quadratureN + 1)) + ((quadratureN + 1) * (quadratureN + 1) + colIndex * (quadratureN + 1)) * (quadratureN + 1)+energyIndex*matLength*matLength] =
-                    make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0);
 
+            //Sets on-shell points for last column
+            for (int row = 0; row < matLength-1; ++row) {
+                potentialClassPtr->V(k[row], k0[energyIndex], coupled, S, J, T, Tz, VArray);
+                setElement(VMatrix,row,matLength-1,energyIndex,matLength,make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0));
             }
+
+            //Sets on-shell points for corner elements
             potentialClassPtr->V(k0[energyIndex], k0[energyIndex], coupled, S, J, T, Tz, VArray);
-            VMatrix[((quadratureN + 1)+ rowIndex * (quadratureN + 1)) + ((quadratureN + 1) * (quadratureN + 1)) + (colIndex * (quadratureN + 1) * (quadratureN + 1))+energyIndex*matLength*matLength] =
-                make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0);
+            setElement(VMatrix,matLength-1,matLength-1,energyIndex,matLength,make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0));
         }
     }
- }
+    }
+}
 
    
