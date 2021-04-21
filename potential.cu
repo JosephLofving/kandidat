9f621f2abeb71a70dfa97e09321d56e3c1283e93
Alexander Korner <r.edna@live.se>
2021-04-21 10:48:11 +0200

    Fix potential loops

--- potential.cu
+++ potential.cu
@@ -103,50 +103,29 @@ void potential(cuDoubleComplex* VMatrix, std::vector<QuantumState> channel, doub
             }
         }
         for (int i = 0; i < matLength * matLength; ++i) {
-            for (int energy_index = 0; energy_index < TLabLength; ++energy_index) {
-                VMatrix[i + (energy_index * matLength * matLength)] = VMat2[i];
+            for (int energyIndex = 0; energyIndex < TLabLength; ++energyIndex) {
+                VMatrix[i + (energyIndex * matLength * matLength)] = VMat2[i];
             }
         }
 
-        for (int kIn = 0; kIn < TLabLength; ++kIn) {
-            for (int kOut = 0; kOut < TLabLength; ++kOut) {
-                potentialClassPtr->V(k0[kIn], k0[kOut], coupled, S, J, T, Tz, VArray);
-                VMatrix[(kIn * (quadratureN + 1) + rowIndex * (quadratureN + 1)) + (kOut * (quadratureN + 1) + colIndex * (quadratureN + 1)) * (quadratureN + 1)] =
-                    make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0);
+        for (int energyIndex=0; energyIndex < TLabLength; ++energyIndex){
+            for (int kOut = 0; kOut < matLength-1; ++kOut) {
+                    potentialClassPtr->V(k0[energyIndex], k[kOut], coupled, S, J, T, Tz, VArray);
+                    VMatrix[((quadratureN + 1) + rowIndex * (quadratureN + 1)) + (kOut * (quadratureN + 1) + colIndex * (quadratureN + 1)) * (quadratureN + 1)+energyIndex*matLength*matLength] =
+                        make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0);
+
             }
+            for (int kIn = 0; kIn < matLength-1; ++kIn) {
+                potentialClassPtr->V(k[kIn], k0[energyIndex], coupled, S, J, T, Tz, VArray);
+                VMatrix[(kIn+ rowIndex * (quadratureN + 1)) + ((quadratureN + 1) * (quadratureN + 1) + colIndex * (quadratureN + 1)) * (quadratureN + 1)+energyIndex*matLength*matLength] =
+                    make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0);
+
+            }
+            potentialClassPtr->V(k0[energyIndex], k0[energyIndex], coupled, S, J, T, Tz, VArray);
+            VMatrix[(quadratureN + 1)+ rowIndex * (quadratureN + 1)) + ((quadratureN + 1) * (quadratureN + 1) + colIndex * (quadratureN + 1)) * (quadratureN + 1)+energyIndex*matLength*matLength] =
+                make_cuDoubleComplex(constants::pi / 2.0 * VArray[arrayIndex], 0);
         }
     }
  }
 
-    /* Define the 1S0 partial-wave quantum numbers 
-    int L = 0; // This argument is actually redundant due to a boolaen "coupled" we use later (which you may see when you compile this program)
-    int S = 0;
-    int J = 0;
-    int T = 1; // I use generalised Pauli principle (L+S+T=odd) to determine T=1 since L=S=0
-    int Tz = 0;
-    */
-
-    /* I set this to be a proton-neutron scattering system, meaning Tz=0.
-     * For pp-scattering we would have Tz=-1 and for nn we would have Tz=+1 */
-
-    
-    /* Array to be filled with potential elements. The ordering of the elements are:
-     * V_array[0]="V_S0": S=0, L=LL=J
-     * V_array[1]="V_S1": S=1, L=LL=J
-     * V_array[2]="V_pp": S=1, L=J+1, LL=J+1
-     * V_array[3]="V_mm": S=1, L=J-1, LL=J-1
-     * V_array[4]="V_pm": S=1, L=J+1, LL=J-1
-     * V_array[5]="V_mp": S=1, L=J-1, LL=J+1
-     * So the 1S0-element will be given by V_array[0] */
-
-    //double* V_array = new double [6];
-
-    /* Our potentials usually use an argument called "coupled", which is a bool-type.
-     * This tells the potential-program which parts the input-array V_array to calculate.
-     * If coupled=true, then it calculates V_S0 and V_S1, otherwise it calculates V_pp, V_mm, V_pm, and V_mp
-     * (since these are coupled states with respect to L!=J and LL!=J) */
-
-    //bool coupled = false;
-    
-    /* Call class-member function V */
-    /* Declare and set in- and out-momenta in c.m.*/
+   
