dae01ae15f9ce8f8cd0b1bc48b0147af68f0ab19
skrompf <erikbz@outlook.com>
2021-04-23 10:49:11 +0200

    fixade koden

--- main.cu
+++ main.cu
@@ -139,6 +139,7 @@ int main() {
 	/* Generate different experimental kinetic energies [MeV]*/
 	for (int i = 0; i < TLabLength; i++) {
 		TLab_h[i] = i * TLabIncr + TLabMin;
+		printf("Tlab[%i] = %.4e", i, TLab_h[i]);
 	}
 
 	//TLab_h[0]=100;
