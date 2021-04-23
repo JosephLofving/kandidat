399011b504091fe8e67880c70d3f907f6c61ed9e
skrompf <erikbz@outlook.com>
2021-04-23 10:50:05 +0200

    fixade koden

--- main.cu
+++ main.cu
@@ -120,7 +120,7 @@ int main() {
 	/* Prepare generation of TLab [Mev] */
 	const double TLabMin = 1;
 	const double TLabMax = 300;
-	constexpr int TLabLength = 400;
+	constexpr int TLabLength = 10;
 	const double TLabIncr = (TLabMax - TLabMin + 1) / TLabLength;
 
 	/* Allocate host memory */
