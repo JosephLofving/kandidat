4500fc176e784f75e1d742de036bd4447adfa8b6
Joself <joseph.lofving@gmail.com>
2021-04-30 10:57:31 +0200

    Städade bort rester från TMatrix-debug

--- computeTMatrix.cu
+++ computeTMatrix.cu
@@ -35,64 +35,30 @@ void* xmalloc(size_t size) {
     return ptr;
 }
 
-// void initSetAMatrix(cuDoubleComplex* mat, double factor) {
-// 	double toSet[N*N] = {4, 3, 9, 3, 7, 7, 0, 5, 8, 6, 1, 8, 9, 4, 2, 9};
-//     for (int i = 0; i < N*N; i++) {
-//         mat[i] = make_cuDoubleComplex(toSet[i]*factor, 0); // Scale each element by the factor and set it
-//     }
-// }
-
-// void initSetBMatrix(cuDoubleComplex* mat, double factor) {
-// 	double toSet[N*N] = {53, 36, 16, 50, 74, 59, 33, 67, 78, 69, 47, 61, 191, 148, 79, 166};
-//     for (int i = 0; i < N*N; i++) {
-//         mat[i] = make_cuDoubleComplex(toSet[i]*factor, 0); // Scale each element by the factor and set it
-//     }
-// }
-
-// Print column-major matrix
-// void printMatrix(cuDoubleComplex* mat, int width, int height) {
-//     for (int i = 0; i < height; i++) {
-//         for (int j = 0; j < width; j++) {
-//             printf("%6.3f ", cuCreal(mat[(j * height) + i]));
-//         }
-//         printf("\n");
-//     }
-//     printf("\n");
-// }
-
 void computeTMatrixCUBLAS(cuDoubleComplex* d_Tarray,
          			cuDoubleComplex* d_Farray, //h_Farray,
 		 			cuDoubleComplex* d_Varray, //h_Varray,
 		 			int matLength, int TLabLength) {
 
-    //const int batchSize{ 1 };
     int batchSize = TLabLength;
 	// cuBLAS variables
     cublasStatus_t status;
     cublasHandle_t handle;
 
     // Host variables
-
-
-    // cuDoubleComplex* h_Farray;
     cuDoubleComplex** h_Fptr_array;
-
-    // cuDoubleComplex* h_Varray;
     cuDoubleComplex** h_Vptr_array;
 
     h_Fptr_array = (cuDoubleComplex**)xmalloc(batchSize * sizeof(cuDoubleComplex*));
     h_Vptr_array = (cuDoubleComplex**)xmalloc(batchSize * sizeof(cuDoubleComplex*));
 
     // Device variables
-    // cuDoubleComplex* d_Farray;
     cuDoubleComplex** d_Fptr_array;
-
-    // cuDoubleComplex* d_Varray;
     cuDoubleComplex** d_Vptr_array;
 
     int* d_pivotArray;
     int* d_trfInfo;
-    int d_trsInfo;
+    int  d_trsInfo;
 
     // Initialize cuBLAS
     status = cublasCreate(&handle);
@@ -101,39 +67,11 @@ void computeTMatrixCUBLAS(cuDoubleComplex* d_Tarray,
         //return (EXIT_FAILURE);
     }
 
-    // Allocate memory for host variables
-    // h_Farray = (cuDoubleComplex*)xmalloc(batchSize *matLength*matLength* sizeof(cuDoubleComplex));
-    // h_Varray = (cuDoubleComplex*)xmalloc(batchSize *matLength*matLength* sizeof(cuDoubleComplex));
-
     // Allocate memory for device variables
-
-    // checkCudaErrors(cudaMalloc((void**)&d_Farray, TLabLength *matLength*matLength* sizeof(cuDoubleComplex)));
-    // checkCudaErrors(cudaMalloc((void**)&d_Varray, TLabLength *matLength*matLength* sizeof(cuDoubleComplex)));
-    checkCudaErrors(
-            cudaMalloc((void**)&d_pivotArray,matLength* TLabLength * sizeof(int)));
+    checkCudaErrors(cudaMalloc((void**)&d_pivotArray,matLength* TLabLength * sizeof(int)));
     checkCudaErrors(cudaMalloc((void**)&d_trfInfo, TLabLength * sizeof(int)));
-    checkCudaErrors(
-            cudaMalloc((void**)&d_Fptr_array, TLabLength * sizeof(cuDoubleComplex*)));
-    checkCudaErrors(
-            cudaMalloc((void**)&d_Vptr_array, TLabLength * sizeof(cuDoubleComplex*)));
-
-    // for (int i = 0; i < batchSize; i++) {
-    //     initSetAMatrix(h_Farray + (i * N*N), (double)(i+1)); // Create matrices scaled by factors 1, 2, ...
-    // }
-
-    // printMatrix(h_Farray, N, N);
-
-    // for (int i = 0; i < batchSize; i++) {
-    //     initSetBMatrix(h_Varray + (i * N*N), (double)(i+1)); // Create matrices scaled by factors 1, 2, ...
-    // }
-
-    // printMatrix(h_Varray, N, N);
-
-    // Copy data to device from host
-    // checkCudaErrors(cudaMemcpy(d_Farray, h_Farray, TLabLength *matLength*matLength* sizeof(cuDoubleComplex),
-                            //    cudaMemcpyHostToDevice));
-    // checkCudaErrors(cudaMemcpy(d_Varray, h_Varray, TLabLength *matLength*matLength* sizeof(cuDoubleComplex),
-                            //    cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMalloc((void**)&d_Fptr_array, TLabLength * sizeof(cuDoubleComplex*)));
+    checkCudaErrors(cudaMalloc((void**)&d_Vptr_array, TLabLength * sizeof(cuDoubleComplex*)));
 
     // Create pointer array for matrices
     for (int i = 0; i < TLabLength; i++) h_Fptr_array[i] = d_Farray + (i * matLength * matLength);
@@ -152,15 +90,13 @@ void computeTMatrixCUBLAS(cuDoubleComplex* d_Tarray,
 								 d_trfInfo, batchSize);
 
 	// Calculate the T matrix
-    status = cublasZgetrsBatched(handle, CUBLAS_OP_N, matLength, matLength, d_Fptr_array, matLength,
-                                 d_pivotArray, d_Vptr_array, matLength, &d_trsInfo,
-								 batchSize);
+    status = cublasZgetrsBatched(handle, CUBLAS_OP_N, matLength, matLength, d_Fptr_array,
+                                matLength, d_pivotArray, d_Vptr_array, matLength, &d_trsInfo,
+								batchSize);
 
     // Copy data to host from device
-    checkCudaErrors(cudaMemcpy(d_Tarray, d_Varray, batchSize *matLength*matLength* sizeof(cuDoubleComplex),
-                               cudaMemcpyDeviceToDevice));
-
-    // printMatrix(h_Varray, N, N);
+    checkCudaErrors(cudaMemcpy(d_Tarray, d_Varray, batchSize*matLength*matLength *
+                            sizeof(cuDoubleComplex), cudaMemcpyDeviceToDevice));
 
     // Free device variables
     checkCudaErrors(cudaFree(d_Fptr_array));
@@ -169,10 +105,6 @@ void computeTMatrixCUBLAS(cuDoubleComplex* d_Tarray,
     checkCudaErrors(cudaFree(d_pivotArray));
     checkCudaErrors(cudaFree(d_Farray));
     checkCudaErrors(cudaFree(d_Varray));
-
-    // Free host variables
-    // if (h_Farray) free(h_Farray);
-    // if (h_Varray) free(h_Varray);
 
     // Destroy cuBLAS handle
     status = cublasDestroy(handle);
